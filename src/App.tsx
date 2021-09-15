d057a31837cdf37bcf5608fe324b587775e5c34c
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-09-15 15:15:13 +0100

    build: added header with logo

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./pages/Home"
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header/>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import logo from "../assets/logo.png"
+
+function Header() {
+    return (
+        <section>
+            <img src={logo} alt="Logo" />
+
+        </section>
+
+
+    )
+}
+
+export default Header
